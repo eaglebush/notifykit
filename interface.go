cb1e59d0359689e3deec1452f9b8f9f18613bb4d
Elizalde Baguinon <mizmerize@yahoo.com>
2026-04-15 14:05:41 +0800

    Update interface.go

--- interface.go
+++ interface.go
@@ -2,8 +2,11 @@ package notifykit
 
 // Message can be the interface for any Go message requirement
 type Message interface {
-	// Encodes the plain text message to base64 for safe JSON value
+	// Encodes the plain text message body to base64 for safe JSON value
 	SetMsg(string) error
+
+	// Decodes the encoded message body from base64
+	GetMsg() string
 }
 
 // Sender can be the interface for any Go notification requirement
